150fdfbd15f7cdb3c4ce33238e1430e0c4af94e9
CadeWeiskopf <weiskopfcd@gmail.com>
2023-03-13 16:27:20 -0400

    testing with other secrets, and more

--- src/ingram_scraper.py
+++ src/ingram_scraper.py
@@ -2,6 +2,14 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+SECRETS_PATH = '/run/secrets/'
+with open(os.path.join(SECRETS_PATH, 'url'), 'r') as url_file:
+    url = url_file.read()
+with open(os.path.join(SECRETS_PATH, 'email'), 'r') as email_file:
+    email = email_file.read()
+with open(os.path.join(SECRETS_PATH, 'password'), 'r') as password_file:
+    password = password_file.read()
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--verbose')
 chrome_options.add_argument('--no-sandbox')
@@ -9,20 +17,11 @@ chrome_options.add_argument('--window-size=1920,1080')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 
+chrome_driver_service = Service('/usr/bin/chromedriver')
 
-chrome_driver_path = '/usr/bin/chromedriver'
-chrome_driver_service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=chrome_driver_service, options=chrome_options)
 
 # scrape
-print('do scrape')
-print('secrets'+str(os.listdir('/run/secrets/')))
-SECRETS_PATH = '/run/secrets/'
-# iterate over files in the directory
-for file_name in os.listdir(SECRETS_PATH):
-    if os.path.isfile(os.path.join(SECRETS_PATH, file_name)):
-        with open(os.path.join(SECRETS_PATH, file_name), 'r') as f:
-            file_contents = f.read()
-            print(f"newFilr: {file_name}\nnewContents:\n{file_contents}\n")
+print(f'do scrape {url} {email} {password}')
 
 driver.quit()
